2d621a11673dcc1daa1a0da83e403afb75f9bc20
David Liang <jlian020@ucr.edu>
2018-07-11 10:26:09 -0700

    Added all the functionalities

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+
+// Crosses out item
+$("ul").on("click", "li", function() {
+	if($(this).css("color") === "rgb(128, 128, 128)") {
+		$(this).css({
+			color: "black",
+			textDecoration: "initial"
+		});
+	}
+	else {
+		$(this).css({
+			color: "grey",
+			textDecoration: "line-through"
+		});
+	}
+});
+
+// Delete item
+$("ul").on("click", ".delete", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// Adding item
+$("input[type='text'").keypress(function(event) {
+	if(event.which === 13) {
+		var userInput = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span class=\"delete\"> X </span>" + userInput + "</li>");
+	}
+});
